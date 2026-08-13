87da4a0efaa4fd4e6008e9ed3a268eda8f7b0811
Sandeep992299 <dissanayakesandeep@gmail.com>
2026-08-13 11:57:27 +0530

    infra: attach CloudWatchAgentServerPolicy to EKS worker node role to permit Container Insights log streaming

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -79,6 +79,11 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOn
   role       = aws_iam_role.node.name
 }
 
+resource "aws_iam_role_policy_attachment" "node_CloudWatchAgentServerPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  role       = aws_iam_role.node.name
+}
+
 # Node Security Group (Exposed so RDS/Redis can filter access)
 resource "aws_security_group" "node" {
   name        = "${var.eks_cluster_name}-node-sg"
