f22d1399a1cd6c43c83a2f2a92b2f895de06f5a9
Sandeep992299 <dissanayakesandeep@gmail.com>
2026-08-13 12:17:07 +0530

    infra: attach CloudWatchReadOnlyAccess policy to EKS node role to permit Grafana to fetch metrics

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -84,6 +84,11 @@ resource "aws_iam_role_policy_attachment" "node_CloudWatchAgentServerPolicy" {
   role       = aws_iam_role.node.name
 }
 
+resource "aws_iam_role_policy_attachment" "node_CloudWatchReadOnlyAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+  role       = aws_iam_role.node.name
+}
+
 # Node Security Group (Exposed so RDS/Redis can filter access)
 resource "aws_security_group" "node" {
   name        = "${var.eks_cluster_name}-node-sg"
